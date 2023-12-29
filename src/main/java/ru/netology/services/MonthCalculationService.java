79c2ac99892e08f14a9f802e8d1a9d1d2eb48383
dasha T <olga061200@icloud.com>
2023-12-29 10:17:50 +0300

    Reformate

--- src/main/java/ru/netology/services/MonthCalculationService.java
+++ src/main/java/ru/netology/services/MonthCalculationService.java
@@ -1,15 +1,16 @@
 package ru.netology.services;
+
 public class MonthCalculationService {
-        public int monthsCalculation (int income, int expenses, int threshold, int money, int count){
-            for (int months = 1; months <= 12; months++) {
-                if (money >= threshold) {
-                    count++;
-                    money = (money - expenses)/3;
-                } else {
-                    money = money + income - expenses;
-                }
+    public int monthsCalculation(int income, int expenses, int threshold, int money, int count) {
+        for (int months = 1; months <= 12; months++) {
+            if (money >= threshold) {
+                count++;
+                money = (money - expenses) / 3;
+            } else {
+                money = money + income - expenses;
             }
-            return (count);
         }
+        return (count);
     }
+}
 
